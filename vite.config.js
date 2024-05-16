b83f42b465e4a8bea015838cbab08e8e6b12beac
PuzanovArtem <puzanov522@gmail.com>
2024-05-16 19:15:29 +0300

    Vite start

---
+++ vite.config.js
@@ -0,0 +1,46 @@
+import Inspect from 'vite-plugin-inspect'
+import path from 'path';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import handlebars from 'vite-plugin-handlebars';
+import { defineConfig } from 'vite';
+import { resolve } from 'path'
+
+export default defineConfig({
+  base: './',
+  plugins: [
+    Inspect(),
+    ViteImageOptimizer({
+      jpg: {
+   
+   quality: 80,
+       },
+       
+       png: {
+         
+         quality: 100,
+       },
+   }),
+   handlebars({
+    partialDirectory: resolve(__dirname, 'src/partials'),
+   }),
+  ],
+  build: {
+    outDir: 'docs',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'login/index.html')
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  esbuild: {
+    jsxFactory: 'create',
+    jsxInject: 'import { create } from "../settings/create.js"',
+  }
+})
+
