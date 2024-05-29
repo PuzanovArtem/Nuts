faa821cee1a1231d0d4a59f549e77db3ec5dc5db
Marta Prokopiv <117581510+Promart1@users.noreply.github.com>
2024-05-29 20:18:57 +0300

    add product section in catalog page

---
+++ catalogPage/catalogPage.js
@@ -0,0 +1,62 @@
+const selected = document.querySelector('.filter__select-selected');
+            const items = document.querySelector('.filter__select-items');
+
+            selected.addEventListener('click', function () {
+                items.style.display = items.style.display === 'block' ? 'none' : 'block';
+            });
+
+            items.addEventListener('click', function (event) {
+                if (event.target && event.target.matches('div[data-value]')) {
+                    selected.textContent = event.target.textContent;
+                    items.style.display = 'none';
+                }
+            });
+
+            document.addEventListener('click', function (event) {
+                if (!event.target.closest('.filter__custom-select')) {
+                    items.style.display = 'none';
+                }
+            });
+
+const select = document.querySelector('.filter__select-selected-second');
+            const item = document.querySelector('.filter__select-items-second');
+
+            select.addEventListener('click', function () {
+                item.style.display = item.style.display === 'block' ? 'none' : 'block';
+            });
+
+            item.addEventListener('click', function (event) {
+                if (event.target && event.target.matches('div[data-value]')) {
+                    select.textContent = event.target.textContent;
+                    item.style.display = 'none';
+                }
+            });
+
+            document.addEventListener('click', function (event) {
+                if (!event.target.closest('.filter__custom-select-second')) {
+                    item.style.display = 'none';
+                }
+            });
+
+
+
+//////load-more
+            
+const loadMoreBtn = document.querySelector('.catalog-page-product__btn');
+const hiddenItems = document.querySelectorAll('.catalog-page-product__item.hidden');
+let hiddenIndex = 0;
+
+
+loadMoreBtn.addEventListener('click', () => {
+    for (let i = 0; i < 3; i++) {
+        if (hiddenIndex < hiddenItems.length) {
+            hiddenItems[hiddenIndex].classList.remove('hidden');
+            hiddenIndex++;
+        }
+    }
+
+    
+    if (hiddenIndex >= hiddenItems.length) {
+        loadMoreBtn.style.display = 'none';
+    }
+});
--- vite.config.js
+++ vite.config.js
@@ -39,7 +39,8 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         customersPage: resolve(__dirname, 'customersPage/index.html'),
-        aboutPage: resolve(__dirname, 'aboutPage/index.html')
+        aboutPage: resolve(__dirname, 'aboutPage/index.html'),
+        catalogPage: resolve(__dirname, 'catalogPage/index.html')
         
       }
     }
