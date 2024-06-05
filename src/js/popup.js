0f268c7c2587c94bd1a3cb9d59c07b84a876713e
PuzanovArtem <puzanov522@gmail.com>
2024-06-05 14:42:42 +0300

    update added galleryPage/ banner . gallery

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,30 +1,33 @@
 var openPopupButtons = document.querySelectorAll('.open-popup-btn');
-    var closePopupButtons = document.querySelectorAll('.popup__close-btn');
+var closePopupButtons = document.querySelectorAll('.popup__close-btn');
 
-    openPopupButtons.forEach(function(button) {
-        button.addEventListener('click', function() {
-            var popupId = this.getAttribute('data-popup');
-            var popup = document.getElementById(popupId);
-            if (popup) {
-                popup.style.display = 'block';
-            }
-        });
+openPopupButtons.forEach(function(button) {
+    button.addEventListener('click', function() {
+        var popupId = this.getAttribute('data-popup');
+        var popup = document.getElementById(popupId);
+        if (popup) {
+            popup.style.display = 'block';
+            document.body.classList.add('no-scroll'); // Додаємо клас для блокування скролінгу
+        }
     });
+});
 
-    closePopupButtons.forEach(function(button) {
-        button.addEventListener('click', function() {
-            var popup = this.closest('.popup');
-            if (popup) {
-                popup.style.display = 'none';
-            }
-        });
+closePopupButtons.forEach(function(button) {
+    button.addEventListener('click', function() {
+        var popup = this.closest('.popup');
+        if (popup) {
+            popup.style.display = 'none';
+            document.body.classList.remove('no-scroll'); // Видаляємо клас для відновлення скролінгу
+        }
     });
+});
 
-    window.addEventListener('click', function(event) {
-        var popups = document.querySelectorAll('.popup');
-        popups.forEach(function(popup) {
-            if (event.target == popup) {
-                popup.style.display = 'none';
-            }
-        });
+window.addEventListener('click', function(event) {
+    var popups = document.querySelectorAll('.popup');
+    popups.forEach(function(popup) {
+        if (event.target == popup) {
+            popup.style.display = 'none';
+            document.body.classList.remove('no-scroll'); // Видаляємо клас для відновлення скролінгу
+        }
     });
+});
